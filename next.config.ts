a13412793a6c98350e078c72eb306563aefa73d7
angkomuhtar <angkomuhtar@gmail.com>
2026-03-27 13:13:02 +0800

    add standalone

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: 10 * 1024 * 1024, // 4 MB
